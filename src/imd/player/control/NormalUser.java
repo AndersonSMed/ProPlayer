89bcbcb82d0ca50e59c281ac05d04285134bdb9c
Yuri-wrlk <yurinatal@hotmail.com>
2016-12-05 17:37:07 -0300

    Commenting done, now a few more tests to be done

--- src/imd/player/control/NormalUser.java
+++ src/imd/player/control/NormalUser.java
@@ -1,11 +1,26 @@
 package imd.player.control;
 
+/**
+ * Extends {@link User} class by setting the differences of privilege of a Normal 
+ * User, a Normal User can play musics from a folder.
+ * @author Anderson Santos and Yuri Reinaldo
+ */
 public class NormalUser extends User{
 
+    /**
+     * Creates a new NormalUser in similar fashion to an {@link User}
+     * @param login this normal user's login
+     * @param password the password that will be set to this normal user
+     */
     public NormalUser(String login, String password){
         super(login, password);
     }
 
+    /**
+     * Overrides the toString method of {@link User} and adds information that
+     * sets a NormalUser apart, Mostly used for saving data.
+     * @return A string with this object's data to be saved.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- src/imd/player/control/VipUser.java
+++ src/imd/player/control/VipUser.java
@@ -1,11 +1,27 @@
 package imd.player.control;
 
+/**
+ * Extends {@link User} class by setting the differences of privilege of a Vip 
+ * User, a Vip User can add other Users, create and use his own playlists and 
+ * all things a Normal User can do.
+ * @author Anderson Santos and Yuri Reinaldo
+ */
 public class VipUser extends User{
     
+    /**
+     * Creates a new VipUser in similar fashion to an {@link User}
+     * @param login
+     * @param password 
+     */
     public VipUser(String login, String password) {
         super(login, password);
     }
     
+    /**
+     * Overrides the toString method of {@link User} and adds information that
+     * sets a VipUser apart, Mostly used for saving data.
+     * @return A string with this object's data to be saved.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
