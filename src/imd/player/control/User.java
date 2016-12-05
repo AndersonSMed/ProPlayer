944e6013ecffd31a791f1dd1137113180f4db030
Yuri-wrlk <yurinatal@hotmail.com>
2016-12-05 17:25:51 -0300

    Comments, comments and comments

--- src/imd/player/control/User.java
+++ src/imd/player/control/User.java
@@ -1,41 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package imd.player.control;
 
 /**
- *
+ * The abstract representation of what parameters and responsibilities a user
+ * should have.
  * @author Anderson Santos and Yuri Reinaldo
  */
 public abstract class User {
+    /**
+     * A {@link String} that keeps the user id, this data should be hidden and
+     * the user must not access it easily.
+     */
     protected String id;
+    /**
+     * A {@link String} that keeps the user login, this should always be set when
+     * creating a new User, since it is it's main key to access the data.
+     */
     protected String login;
+    /**
+     * A {@link String} that keeps the user password, it must be set when creating
+     * objects of this class and should not be easily accessed.
+     */
     protected String password;
     
+    /**
+     * A simple constructor who sets the variables and creates a new id for the 
+     * user.
+     * @param login The login to be set.
+     * @param password The password to be set.
+     */
     public User(String login, String password){
         this.id = login.hashCode() + "";
         this.login = login;
         this.password = password;
     }
 
+    /**
+     * Returns the id of this User object, it is used in order to access data
+     * related to this User.
+     * @return a {@link String} representing this User's id. 
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns the login of this User, this is mostly used to access other user's
+     * objects.
+     * @return a {@link String} with this user login. 
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * Returns this users password, should be used only for user verification
+     * should be used sparingly.
+     * @return 
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets a new password to this user, used mostly for updating user data.
+     * @param password this user's new password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Turns this user data in a {@link String} in order to be saved in a backup.
+     * @return A {@link String} that contains this user's login and password in
+     * separate lines.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- src/imd/player/control/UserControl.java
+++ src/imd/player/control/UserControl.java
@@ -4,17 +4,31 @@ import imd.player.model.ModelFacade;
 import java.util.ArrayList;
 
 /**
- *
+ * Manages most of {@link User} related methods, including {@link VipUser} and
+ * {@link NormalUser} implementations.
  * @author Anderson Santos and Yuri Reinaldo
  */
 public class UserControl {
 
+    /**
+     * The {@link User} instance who is set to be the main User of this program 
+     * usage.
+     */
     private User loggedUser;
 
+    /** 
+     * Simple constructor, not much is done here.
+     */
     public UserControl() {
 
     }
 
+    /**
+     * Tries to make a login attempt given the passed login and password.
+     * @param login The login which is used to identify the user.
+     * @param password The password used to verify this user identity.
+     * @return True if both fields match a User registered, False otherwise.
+     */
     public boolean loginAttempt(String login, String password) {
         User user;
         try {
@@ -35,6 +49,13 @@ public class UserControl {
         return false;
     }
 
+    /**
+     * Adds a new {@link User} with {@link NormalUser} privileges. 
+     * @param login the new user login, cannot be set to another value later.
+     * @param password the new user's first password.
+     * @return True if the user is a new one and could be saved in the database,
+     * False if a user with the same login already exists.
+     */
     public boolean addNormalUser(String login, String password) {
         if (this.isAdmin()) {
             NormalUser user = new NormalUser(login, password);
@@ -47,6 +68,13 @@ public class UserControl {
         return false;
     }
 
+    /**
+     * Adds a new {@link User} with {@link VipUser} privileges. 
+     * @param login the new user login, cannot be set to another value later.
+     * @param password the new user's first password.
+     * @return True if the user is a new one and could be saved in the database,
+     * False if a user with the same login already exists.
+     */
     public boolean addVipUser(String login, String password) {
         if (this.isAdmin()) {
             VipUser user = new VipUser(login, password);
@@ -59,6 +87,11 @@ public class UserControl {
         return false;
     }
 
+    /**
+     * Adds a new {@link Playlist} to the passed User's collection
+     * @param name The user who owns this playlist
+     * @param musics The array of musics who will be inserted into the playlist
+     */
     public void addPlaylist(String name, ArrayList<Music> musics) {
         if (this.isAdmin()) {
             try {
@@ -69,6 +102,11 @@ public class UserControl {
         }
     }
 
+    /**
+     * Removes a {@link Playlist} from the logged user's collection
+     * @param name the name of the string which should be removed
+     * @return True if the playlist was found and was removed, False otherwise.
+     */
     public boolean removePlaylist(String name) {
         if (this.isAdmin()) {
             try {
@@ -80,10 +118,18 @@ public class UserControl {
         return false;
     }
 
+    /**
+     * Returns whether the logged user has {@link VipUser} privileges
+     * @return True if the logged user is a {@link VipUser}, False otherwise
+     */
     public boolean isAdmin() {
         return (this.loggedUser instanceof VipUser);
     }
 
+    /**
+     * Returns the instance of the logged user, used for many verifications.
+     * @return the instance of the actual user
+     */
     public User getLoggedUser() {
         return this.loggedUser;
     }
